4c253adba04691b31390089f03746e90ec151df3
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-23 17:06:25 -0500

    Put singers behind neat cards with accordions :)

--- src/components/MaskedSingers/MaskedSingers.js
+++ src/components/MaskedSingers/MaskedSingers.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import './SingerStyles.css';
 import Unmask from '../Unmask/Unmask';
-import { Accordion, AccordionPanel, Box, Text } from 'grommet';
+import {
+  Grommet,
+  Card,
+  CardBody,
+  CardHeader,
+  Text,
+  Accordion,
+  AccordionPanel,
+  Box,
+} from 'grommet';
 
 const SINGERS = gql`
   {
@@ -20,21 +29,11 @@ const SINGERS = gql`
   }
 `;
 
-// function Unmasks() {
-//   const { loading, error, data } = useQuery(SINGERS);
-
-//   return (
-//     <div key={id}>
-//       <button className='btn'>Unmask {maskedname} </button>
-//     </div>
-//   );
-// }
-
 export default function MaskedSingers() {
   const { loading, error, data } = useQuery(SINGERS);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error 😇</p>;
+  if (error) return <p>Error 😑</p>;
 
   return data.MaskedSingers.map(
     ({
@@ -48,8 +47,53 @@ export default function MaskedSingers() {
       unmaskedimageurl,
       winner,
     }) => (
-      <div key={id} className='monster-box'>
-        <p className='attribute'>
+      <div key={id} className='content'>
+        <Grommet>
+          <Card
+            margin={{ bottom: 'large' }}
+            background={{ dark: true, color: 'dark-1' }}
+            gap='xxsmall'
+            fill='horizontal'
+            width={{ min: 'medium', max: 'medium', width: '100%' }}
+            height={{ min: 'large', max: 'medium', height: '100%' }}
+            overflow='visible'
+            hoverIndicator={{ opacity: 'strong' }}
+          >
+            <CardBody
+              pad='small'
+              background={{
+                image: `url(${maskedimageurl})`,
+              }}
+            >
+              <CardHeader
+                align='center'
+                direction='row'
+                flex={false}
+                justify='center'
+                gap='medium'
+                pad='small'
+              >
+                {maskedname}
+              </CardHeader>
+            </CardBody>
+            <Accordion>
+              <AccordionPanel
+                label='Unmask This Singer!'
+                className='accordion'
+                alignSelf='center'
+              >
+                <Box pad='large' flex='true'>
+                  <Text>{contestant}</Text>
+                  <p className='attribute'>
+                    <img src={unmaskedimageurl} alt='img' className='pic'></img>
+                  </p>
+                </Box>
+              </AccordionPanel>
+            </Accordion>
+          </Card>
+        </Grommet>
+
+        {/* <p className='attribute'>
           <h2 className='text-gradient text-glow bigbold'>Creature</h2>
           <h3 className='attribute'> {maskedname} </h3>
         </p>
@@ -60,18 +104,23 @@ export default function MaskedSingers() {
         <p className='attribute'>Debut Season: {season}</p>
         <p className='attribute'>Occupation: {occupation} </p>
         <p className='attribute'>Winner??: {winner} </p>
-        <p className='attribute'>Dated Voted Off: {datevotedoff} </p>
+        <p className='attribute'>Dated Voted Off: {datevotedoff} </p> */}
 
-        <Accordion>
-          <AccordionPanel label='Unmask This Singer!' className='accordion'>
-            <Box pad='medium'>
+        {/* <Accordion>
+          <AccordionPanel
+            label='Unmask This Singer!'
+            className='accordion'
+            alignSelf='center'
+          >
+            <Box pad='large' flex='true'>
               <Text>{contestant}</Text>
               <p className='attribute'>
                 <img src={unmaskedimageurl} alt='img' className='pic'></img>
               </p>
             </Box>
           </AccordionPanel>
-        </Accordion>
+        </Accordion> */}
+
         {/* <button className='btn'>Unmask {maskedname}! </button> */}
       </div>
     )
