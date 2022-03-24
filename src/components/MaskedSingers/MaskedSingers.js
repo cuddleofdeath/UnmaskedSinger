0f5a026eeda7c45da56ba6384f89ebe125ca4953
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-24 12:26:24 -0500

    Removed unused imports/code + added classNames

--- src/components/MaskedSingers/MaskedSingers.js
+++ src/components/MaskedSingers/MaskedSingers.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import './SingerStyles.css';
-import Unmask from '../Unmask/Unmask';
 import {
   Grommet,
   Card,
@@ -32,8 +31,8 @@ const SINGERS = gql`
 export default function MaskedSingers() {
   const { loading, error, data } = useQuery(SINGERS);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error 😑</p>;
+  if (loading) return <p className='loading'>Loading...</p>;
+  if (error) return <p className='error'>Error 😑</p>;
 
   return data.MaskedSingers.map(
     ({
@@ -97,36 +96,6 @@ export default function MaskedSingers() {
             </Accordion>
           </Card>
         </Grommet>
-
-        {/* <p className='attribute'>
-          <h2 className='text-gradient text-glow bigbold'>Creature</h2>
-          <h3 className='attribute'> {maskedname} </h3>
-        </p>
-
-        <p className='attribute'>
-          <img src={maskedimageurl} alt='img' className='pic'></img>
-        </p>
-        <p className='attribute'>Debut Season: {season}</p>
-        <p className='attribute'>Occupation: {occupation} </p>
-        <p className='attribute'>Winner??: {winner} </p>
-        <p className='attribute'>Dated Voted Off: {datevotedoff} </p> */}
-
-        {/* <Accordion>
-          <AccordionPanel
-            label='Unmask This Singer!'
-            className='accordion'
-            alignSelf='center'
-          >
-            <Box pad='large' flex='true'>
-              <Text>{contestant}</Text>
-              <p className='attribute'>
-                <img src={unmaskedimageurl} alt='img' className='pic'></img>
-              </p>
-            </Box>
-          </AccordionPanel>
-        </Accordion> */}
-
-        {/* <button className='btn'>Unmask {maskedname}! </button> */}
       </div>
     )
   );
