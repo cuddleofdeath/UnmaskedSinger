be4912664b93191bc95c440e7b0bd17b7ba4c39f
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-26 20:58:00 -0500

    Fixed Navbar Component styling

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -5,11 +5,13 @@ import { GiCarnivalMask } from 'react-icons/gi';
 
 function NavigationBar() {
   return (
-    <div className='NavBar'>
+    <div className='Navbar'>
       <Navbar bg='dark' fixed='top'>
         <Navbar.Brand className='brand'>
-          <GiCarnivalMask className='icon' />
-          <h3 className='logo'> The Unmasked Singer</h3>
+          <h3>
+            <GiCarnivalMask className='icon' />
+            <logo className='logo'>The Unmasked Singer</logo>
+          </h3>
         </Navbar.Brand>
       </Navbar>
     </div>
