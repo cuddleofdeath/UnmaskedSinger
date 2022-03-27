28f74851a2cc0085bf6ad4664059fefc9fd2fd35
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-26 22:35:32 -0500

    fixed navbar styling

--- src/components/SeasonSelector/SeasonSelector.js
+++ src/components/SeasonSelector/SeasonSelector.js
@@ -2,4 +2,8 @@ export const selectSeason = [
   { title: 'Season 1' },
   { title: 'Season 2' },
   { title: 'Season 3' },
+  { title: 'Season 4' },
+  { title: 'Season 5' },
+  { title: 'Season 6' },
+  { title: 'Season 7' },
 ];
