c5e1bf6d14976fd032a0d7dec2d1a13db9f1e7fb
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-30 03:50:31 -0500

    Restructured app, added routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import Season1 from './components/MaskedSingers/Season 1/Season 1';
+// import './App.css';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import MainPage from './components/MaskedSingers/MainPage/MainPage';
+
+import Footer from './components/Footer/Footer';
+import Season2 from './components/MaskedSingers/Season 2/Season2';
+import Season3 from './components/MaskedSingers/Season 3/Season3';
+import Season4 from './components/MaskedSingers/Season 4/Season4';
+import Season5 from './components/MaskedSingers/Season 5/Season5';
+import Season6 from './components/MaskedSingers/Season 6/Season 6';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/season1' element={<Season1 />} />
+        <Route path='/season2' element={<Season2 />} />
+        <Route path='/season3' element={<Season3 />} />
+        <Route path='/season4' element={<Season4 />} />
+        <Route path='/season5' element={<Season5 />} />
+        <Route path='/season6' element={<Season6 />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
