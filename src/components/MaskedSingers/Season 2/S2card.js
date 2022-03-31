3be7987e7750039e0e56f2797ac75c2d401bdb02
cuddleofdeath <cuddleofdeath@gmail.com>
2022-03-31 01:28:41 -0500

    setup card grids for each season

---
+++ src/components/MaskedSingers/Season 2/S2card.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { SINGERS_2 } from '../../Fragments/Fragments';
+import { Typography } from 'antd';
+
+import Collapsible from 'react-collapsible';
+
+export default function S2card() {
+  const { loading, error, data } = useQuery(SINGERS_2);
+
+  return data.MaskedSingers.map(
+    ({
+      contestant,
+      datevotedoff,
+      id,
+      maskedimageurl,
+      maskedname,
+      occupation,
+      season,
+      unmaskedimageurl,
+      winner,
+    }) => (
+      <div className='card'>
+        <div className='card__body'>
+          <img src={maskedimageurl} alt='' className='card__image' />
+          <h2 className='card__title'>{maskedname}</h2>
+          <p className='card__description'>Who do you think {maskedname} is?</p>
+        </div>
+        <Collapsible
+          trigger={<button className='card__btn'>Unmask this singer</button>}
+        >
+          <h2 className='card__title'>{contestant}</h2>
+          <img src={unmaskedimageurl} alt='' className='card__image' />
+
+          <Typography className='card__description'>
+            Occupation: {occupation}
+          </Typography>
+          <Typography className='card__description'>
+            Date Voted Off: {datevotedoff}
+          </Typography>
+          <Typography className='card__description'>
+            Season {season} Winner?: {winner}
+          </Typography>
+        </Collapsible>
+      </div>
+    )
+  );
+}
--- src/components/MaskedSingers/Season 7/Season7.js
+++ src/components/MaskedSingers/Season 7/Season7.js
@@ -14,23 +14,21 @@ import {
   createTheme,
   ThemeProvider,
 } from '@mui/material';
-
 import { Card, CardGroup, Button } from 'react-bootstrap';
 import { useQuery } from '@apollo/client';
-import { SINGERS_6 } from '../../Fragments/Fragments';
+import { SINGERS_7 } from '../../Fragments/Fragments';
+import './Season7.scss';
+import S7card from './S7card';
 
-import '../Season 7/Season7.scss';
-import S6card from './S6card';
-
-export default function Season6() {
-  const { loading, error, data } = useQuery(SINGERS_6);
+export default function Season7() {
+  const { loading, error, data } = useQuery(SINGERS_7);
 
   if (loading) return <p className='loading'>Loading...</p>;
   if (error) return <p className='error'>Error 😑</p>;
 
   return (
     <div className='wrapper'>
-      <S6card />
+      <S7card />
     </div>
   );
 }
